7a2e9e75de5d640fc7d8cfa78213907266ff4374
Matthew Norton <matthew.lee.norton@gmail.com>
2022-10-31 14:52:05 -0600

    commit1

---
+++ getcredit/creditaccess/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import indexPageView
+from .views import aboutPageView
+from .views import contactPageView
+
+urlpatterns = [
+    path("", indexPageView, name="index"),
+    path("about/", aboutPageView, name="about"),
+    path("contact/", contactPageView, name="contact"),
+    ]
--- getcredit/creditaccess/views.py
+++ getcredit/creditaccess/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def indexPageView(request) :
+    return HttpResponse('Welcome to our project')
+
+def aboutPageView(request) :
+    return HttpResponse('About Us')
+
+def contactPageView(request) :
+    return HttpResponse('Contact Us')
