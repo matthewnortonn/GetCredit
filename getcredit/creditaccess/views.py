8d521076f2be438b8ea18169e4a10d4ae06d1cda
Matthew Norton <matthew.lee.norton@gmail.com>
2022-11-15 16:37:31 -0700

    Co-authored-by: chadyorgason <chadyorgason@users.noreply.github.com>

--- getcredit/creditaccess/views.py
+++ getcredit/creditaccess/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    return HttpResponse('Welcome to our project')
+    return render(request, 'base.html')
+    #return HttpResponse('Welcome to our project')
 
 def aboutPageView(request) :
     return HttpResponse('About Us')
